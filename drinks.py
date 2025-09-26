97369cf5e08770a7d0267e501f1f1fcc41b6015d
Maria Guillen <mguillen@catalyte.io>
2025-09-26 14:48:44 -0500

    created dictionary with drink names and markup percentage

--- drinks.py
+++ drinks.py
@@ -1,3 +1,11 @@
+drink_menu = [
+    {"drinkName": "Black Coffee", "markupPrice": 0.30},
+    {"drinkName": "MNM Latte", "markupPrice": 0.20},
+    {"drinkName": "Espresso", "markupPrice": 0.20},
+    {"drinkName": "Hot Chocolate", "markupPrice": 0.20},
+]
+
+
 class Drinks:
 
     def __init__(self, name, ingredients, cost_to_produce, markup_percentage):
@@ -5,3 +13,6 @@ class Drinks:
         self.ingredients = ingredients
         self.produce_cost = cost_to_produce
         self.markup = markup_percentage
+
+    def calculate_sale_price(self):
+        sale_price = self.produce_cost + (self.produce_cost * 0.25)
