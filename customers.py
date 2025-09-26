ede3076ee574f9a0fd4a0b3fe5996fc14e3a4b54
Jaluan Newson <jnewson@catalyte.io>
2025-09-26 13:43:34 -0500

    added dictionary to use for CRUD

--- customers.py
+++ customers.py
@@ -1,9 +1,19 @@
 class EspressOCustomer(): 
 
-    def __init__(self, customer_name, email, lifetime_spent): 
+    def __init__(self, customer_name: str, email: str, lifetime_spent: float): 
         
         self.customer_name = customer_name
         self.email = email
         self.lifetime_spent = lifetime_spent
 
-        
+    def to_dict(self): 
+          """Return a dictionary representation of the customer."""
+          
+          return {
+
+            "customer_name": self.customer_name, 
+            "email": self.email,
+            "lifetime_spent": self.lifetime_spent 
+        }
+    
+    
