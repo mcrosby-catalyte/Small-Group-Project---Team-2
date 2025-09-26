447bd73b6ad6dc002cbea8201f5ec7faa96e44be
Maria Guillen <mguillen@catalyte.io>
2025-09-26 14:58:38 -0500

    edited drink names

--- drinks.py
+++ drinks.py
@@ -1,5 +1,5 @@
 drink_menu = [
-    {"drinkName": "Black Coffee", "markupPrice": 0.30},
+    {"drinkName": "Black Coffee", "markupPercentage": 0.30},
     {"drinkName": "MNM Latte", "markupPrice": 0.20},
     {"drinkName": "Espresso", "markupPrice": 0.20},
     {"drinkName": "Hot Chocolate", "markupPrice": 0.20},
@@ -15,4 +15,6 @@ class Drinks:
         self.markup = markup_percentage
 
     def calculate_sale_price(self):
-        sale_price = self.produce_cost + (self.produce_cost * 0.25)
+        sale_price = purchasing_cost + (
+            purchasing_cost * drink_menu["markupPercentage"]
+        )
