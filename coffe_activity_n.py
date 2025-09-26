e4589fa3af282ff6b83fd6ed9ca48f821f30d674
Noah Smith <nsmith@catalyte.io>
2025-09-26 11:21:12 -0500

    making classes

---
+++ coffe_activity_n.py
@@ -0,0 +1,9 @@
+# Baked Good
+# name
+# purchasing cost
+# markup percentage
+# vendor name
+# list of known allergens (ex. wheat, peanuts, milk)
+# sale price (calculated from purchasing cost + markup percentage when created
+
+Class BakedGoods():
