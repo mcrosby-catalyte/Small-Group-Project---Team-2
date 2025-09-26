a785fab61d2d1b1f20ca41f4aa7f03b9a9178489
Noah Smith <nsmith@catalyte.io>
2025-09-26 11:44:04 -0500

    figuring out item names

--- coffe_activity_n.py
+++ coffe_activity_n.py
@@ -6,4 +6,21 @@
 # list of known allergens (ex. wheat, peanuts, milk)
 # sale price (calculated from purchasing cost + markup percentage when created
 
-Class BakedGoods():
+
+class bakedGood:
+    def __int__(
+        self, name, purchasing_cost, markup_percentage, vendor_name, sale_price
+    ):
+        self.name = name
+        self.name = markup_percentage
+        self.purchasing_cost = purchasing_cost
+        self.markup_percentage = vendor_name
+        self.vedor_name = vendor_name
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+bread = bakedGood("bread", 5.50, 0.20, "shop")
+
+print(bread)
