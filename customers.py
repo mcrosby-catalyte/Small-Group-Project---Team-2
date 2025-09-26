7b4b491ca79113db0e42db671fe8f7f21195eb42
Jaluan Newson <jnewson@catalyte.io>
2025-09-26 13:22:39 -0500

    added self.

--- customers.py
+++ customers.py
@@ -2,3 +2,8 @@ class EspressOCustomer():
 
     def __init__(self, customer_name, email, lifetime_spent): 
         
+        self.customer_name = customer_name
+        self.email = email
+        self.lifetime_spent = lifetime_spent
+
+        
