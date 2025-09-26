abaf546489df466af5cf391c229b9cf378bc06b3
Maria Guillen <mguillen@catalyte.io>
2025-09-26 13:43:00 -0500

    created class and constructor

---
+++ drinks.py
@@ -0,0 +1,7 @@
+class Drinks:
+
+    def __init__(self, name, ingredients, cost_to_produce, markup_percentage):
+        self.name = name
+        self.ingredients = ingredients
+        self.produce_cost = cost_to_produce
+        self.markup = markup_percentage
