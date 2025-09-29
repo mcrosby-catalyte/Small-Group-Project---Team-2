6616977486da6b7a0cf40f52ec84a18f274c37e6
Noah Smith <nsmith@catalyte.io>
2025-09-29 14:52:15 -0500

    added allergen list

--- baked_goods_repository.py
+++ baked_goods_repository.py
@@ -1,12 +1,3 @@
-# Baked Good
-# name
-# purchasing cost
-# markup percentage
-# vendor name
-# list of known allergens (ex. wheat, peanuts, milk)
-# sale price (calculated from purchasing cost + markup percentage when created
-
-
 class bakedGood:
     """
     Gives attributes to food items.
@@ -27,11 +18,14 @@ class bakedGood:
         self.markup_percentage = markup_percentage
         self.purchasing_cost = purchasing_cost
         self.vedor_name = vendor_name
-        self.sale_price = sale_price
+        self.sale_price = self.calculate_sale_price()
         self.allergens = allergens
 
+    def calculate_sale_price(self):
+        return round(self.purchasing_cost * (1 + self.markup_percentage), 2)
+
     def __str__(self):
-        return f"{self.name}\nsale price: {self.sale_price}\nallergen: {self.allergens}"
+        return f"\n{self.name}\nsale price: {self.sale_price}\nallergen: {self.allergens}\n\n"
 
 
 cinnamon_roll = bakedGood("Cinnamon roll", 5.50, 0.20, "shop", 10, "wheat")
@@ -40,7 +34,16 @@ coffee_cake = bakedGood("Coffe Cake", 5.50, 0.20, "shop", 5, "wheat")
 croissant = bakedGood("croissant", 5.50, 0.20, "shop", 5, "wheat")
 bread = bakedGood("bread", 5.50, 0.20, "shop", 5, "wheat")
 
-
 baked_list = [cinnamon_roll, muffin, coffee_cake, croissant, bread]
 
-print(coffee_cake)
+allergens_list = [
+    "milk",
+    "eggs",
+    "wheat",
+    "soy",
+    "peanuts",
+    "tree nuts",
+    "fish",
+    "shellfish",
+    "sesame",
+]
