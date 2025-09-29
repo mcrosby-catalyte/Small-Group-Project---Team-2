7a845a3b07fb57d5bdd765f79a057cd33d7bf23d
Jaluan Newson <jnewson@catalyte.io>
2025-09-29 10:53:05 -0500

    imported customer file

--- loyalty_program.py
+++ loyalty_program.py
@@ -2,4 +2,10 @@
 #The customer gains 10 points per $1.00 spent and tracked by the customers phone number 
 #Will need to import customers.py file to use customer instances and data. 
 
+import customers
+
+
 class loyalty_program(): 
+
+    def __init__(self, phone, email, lifetime_spent): 
+        
